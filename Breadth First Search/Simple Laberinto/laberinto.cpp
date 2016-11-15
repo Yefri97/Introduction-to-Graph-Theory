732571b00659161eefb22b8223234e2a3b77b721
Catalina Castro <Catalina Castro>
2016-11-15 11:42:29 -0500

    Laberinto Simple 99%

--- Breadth First Search/Simple Laberinto/laberinto.cpp
+++ Breadth First Search/Simple Laberinto/laberinto.cpp
@@ -1,95 +1,116 @@
 #include <windows.h>
-#include <bits/stdc++.h>
 #include <vector>
 #include <queue>
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
 
 using namespace std;
 
-void gotoxy(int x, int y, char c){
-	HANDLE hCon;
-	hCon = GetStdHandle(STD_OUTPUT_HANDLE);
-	COORD dwPos;
-	dwPos.X = x;
-	dwPos.Y = y;
-	SetConsoleCursorPosition(hCon, dwPos);
-	cout << c;
+void gotoxy(int x, int y){
+  HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+  COORD dwPos = {x, y + 1};
+  SetConsoleCursorPosition(hCon, dwPos);
 }
 
-struct state {
-	int r, c;
-	state() {}
-	state(int _r, int _c) : r(_r), c(_c) {}
+void hideCursor() {
+  HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_CURSOR_INFO cci;
+  cci.dwSize = 2;
+  cci.bVisible = false;
+  SetConsoleCursorInfo(hCon, &cci);
+}
+
+struct node {
+  int i, j;
+  node() {}
+  node(int _i, int _j) : i(_i), j(_j) {}
 };
 
 const int MX = 100, UNVISITED = -1;
-int n, m;
-state  path[MX + 10][MX + 10];
+int height, width;
+node path[MX + 10][MX + 10];
 char board[MX + 10][MX + 10];
 
-int mi[10] = {0, -1, 0, 1};
-int mj[10] = {-1, 0, 1, 0};
+int movi[10] = {0, -1, 0, 1};
+int movj[10] = {-1, 0, 1, 0};
 
-int breadthFirstSearch(state s, state t) {
-	vector< vector<int> > dist(n, vector<int>(m, UNVISITED)); dist[s.r][s.c] = 0;
-  queue<state> q;	q.push(s);
-  while (!q.empty()) {
-		state u = q.front(); q.pop();
-	  for (int j = 0; j < 4; j++) {
-			state v = state(u.r + mi[j], u.c + mj[j]);
-			if (v.r < 0 || v.r >= n || v.c < 0 || v.c >= m) v = u;
-			if (board[v.r][v.c] == '#') v = u;
-			if (dist[v.r][v.c] == UNVISITED) {
-				dist[v.r][v.c] = dist[u.r][u.c] + 1;
-				path[v.r][v.c] = u;
-				q.push(v);
-			}
-	  }
+int breadthFirstSearch(node s, node t) {  // BFS
+  // Inicializamos todos los nodos como NO VISITADOS, excepto el Source
+  vector< vector<int> > dist(height, vector<int>(width, UNVISITED)); dist[s.i][s.j] = 0;
+  queue<node> q; q.push(s); // Creamos la cola, y la inicializamos con Source
+  while (!q.empty()) { // Mientras mi cola no este vacia
+    node u = q.front(); q.pop(); // Extraemos el nodo U del frente de la cola
+    for (int k = 0; k < 4; k++) { // Exploramos todos sus vecinos
+      node v = node(u.i + movi[k], u.j + movj[k]);  // Vecino V
+      if (v.i < 0 || v.i >= height || v.j < 0 || v.j >= width) v = u;
+      if (board[v.i][v.j] == '#') v = u;
+      if (dist[v.i][v.j] == UNVISITED) {  // Si ese nodo no ha sido visitado
+        dist[v.i][v.j] = dist[u.i][u.j] + 1;  // Lo marcamos como visitado
+        path[v.i][v.j] = u; // Guardamos el padre para reconstruir la respuesta
+        q.push(v);  // Insertamos el nodo a la cola
+      }
+    }
   }
-  return dist[t.r][t.c];
+  return dist[t.i][t.j]; // Retornamos la distancia mas corta al Target
 }
 
 int main() {
-	ifstream cin("laberinto.txt");
-	state source, target;
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			char c; cin >> c;
-			if (c == 'E') source = state(i, j);
-			if (c == 'S') target = state(i, j);
-			board[i][j] = c;
-		}
-	}
 
-	int lengthPath = breadthFirstSearch(source, target);
+  // Leyendo de archivo
+  ifstream cin("laberinto2.in");
 
-	state curr;
-	curr = target;
-	vector<state> ans(lengthPath + 1);
-	for (int i = lengthPath; i >= 1; i--) {
-		ans[i] = curr;
-		curr = path[curr.r][curr.c];
-	}
-	ans[0] = source;
+  // Crea el estado inicial y el objetivo
+  node source, target;
 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			gotoxy(j, i, board[i][j]);
+  // Lee las dimensiones del tablero
+  cin >> height >> width;
 
-	cout << lengthPath << endl;
+  // Lee el tablero
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      char c; cin >> c;
+      if (c == 'E') source = node(i, j);
+      if (c == 'S') target = node(i, j);
+      board[i][j] = c;
+    }
+  }
 
-	curr = ans[1];
-	for (int i = 1; i < ans.size(); i++) {
-		gotoxy(curr.c, curr.r, '.');
-		curr = ans[i];
-		gotoxy(curr.c, curr.r, '*');
-		Sleep(800);
-	}
+  // Halla el camino mas corto del source al target mediante un BFS
+  int lengthPath = breadthFirstSearch(source, target);
 
-	gotoxy(0, n, ' ');
-	getchar();
+  // Reconstruyendo la respuesta
+  node curr;
+  curr = target;
+  vector<node> ans(lengthPath);
+  for (int i = lengthPath - 1; i >= 0; i--) {
+    ans[i] = curr;
+    curr = path[curr.i][curr.j];
+  }
+
+  cout << "La longitud del camino mas corto es " << lengthPath << endl;
+
+  // Dibujando el tablero
+  hideCursor();
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; j < width; j++) {
+      gotoxy(j, i);
+      cout << board[i][j];
+    }
+  }
+
+  // Mostrando las respuesta
+  curr = ans[0];
+  gotoxy(curr.j, curr.i); cout << "*";
+  for (int i = 0; i < ans.size(); i++) {
+    gotoxy(curr.j, curr.i); cout << ".";
+    curr = ans[i];
+    gotoxy(curr.j, curr.i); cout << "*";
+    Sleep(200);
+  }
+
+  gotoxy(0, height);
+  getchar();
 
   return 0;
 }
